627b4f4a91fae9c5be821daf7cfca29a12cf3c05
Leif Anderson <leif.v.anderson@gmail.com>
2017-08-11 18:17:49 -0500

    rgr

---
+++ rgr/rgr.rb
@@ -0,0 +1,15 @@
+require_relative 'myclass'
+RSpec.describe MyClass do
+  # incomplete ...
+  before(:each) do
+        @account = Account.new
+        @account.deposit_checking(100.00)
+        @account.deposit_savings(20.00)
+  end
+  it "can't retireve the users checking account balance" do
+    expect(@account.get_checking_balance).to be > 0
+  end
+  it "can't retrieve the users total account balance" do
+
+  end
+end
